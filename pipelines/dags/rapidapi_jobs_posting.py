cc23aae330bcc85ad9816852e7911342a25ef4bf
SaschaKay <kayris.alexandra@gmail.com>
2025-04-18 18:49:18 +0000

    init commit airflow

---
+++ pipelines/dags/rapidapi_jobs_posting.py
@@ -0,0 +1,37 @@
+import sys
+import os
+
+CURRENT_DIRECTORY = os.path.dirname(__file__)
+PIPELINES_ROOT = os.path.join(CURRENT_DIRECTORY, '..')
+if PIPELINES_ROOT not in sys.path:
+    sys.path.append(PIPELINES_ROOT)
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+from rapidapi_jobs_posting import pipeline_load, pipeline_transform
+
+default_args = {
+    'start_date': datetime(2024, 1, 18),
+    'retries': 0,
+}
+
+with DAG(
+    dag_id='jobs_postings_pipeline',
+    default_args=default_args,
+    schedule_interval='0 6 * * *',
+    catchup=False,
+    tags=['etl'],
+) as dag:
+
+    load_task = PythonOperator(
+        task_id='jobs_postings_load',
+        python_callable=pipeline_load.main,
+    )
+
+    transform_task = PythonOperator(
+        task_id='job_postings_transform',
+        python_callable=pipeline_transform.main,
+    )
+
+    load_task >> transform_task
---
+++ pipelines/dags/readme.txt
@@ -0,0 +1,2 @@
+should be mounted in /opt/airflow/dags
+sudo ln -s /opt/airflow/repos/jobs-research/pipelines/dags /opt/airflow/dags
